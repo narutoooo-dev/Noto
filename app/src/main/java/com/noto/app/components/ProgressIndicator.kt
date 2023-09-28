746f4b37cc72211e6b7eb558ffd6a8da55158542
Ali Albaali <ali@albaali.com>
2023-09-27 21:51:32 -0400

    Implement ActionCircularProgressIndicator to be used in the top app bar

--- app/src/main/java/com/noto/app/components/ProgressIndicator.kt
+++ app/src/main/java/com/noto/app/components/ProgressIndicator.kt
@@ -9,15 +9,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 
+private val CircularProgressIndicatorStrokeWidth = 5.dp
+private val ScreenProgressIndicatorSize = 50.dp
+private val ActionProgressIndicatorSize = 24.dp
+private val DefaultStrokeCap = StrokeCap.Round
+
 @Composable
-fun ProgressIndicator(modifier: Modifier = Modifier, color: Color = MaterialTheme.colorScheme.primary) {
+fun ScreenCircularProgressIndicator(modifier: Modifier = Modifier, color: Color = MaterialTheme.colorScheme.primary) {
     Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         CircularProgressIndicator(
-            modifier = Modifier.size(50.dp),
+            modifier = Modifier.size(ScreenProgressIndicatorSize),
             color = color,
-            strokeWidth = 5.dp,
+            strokeWidth = CircularProgressIndicatorStrokeWidth,
         )
     }
+}
+
+@Composable
+fun ActionCircularProgressIndicator(
+    progress: Float,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.primary,
+    trackColor: Color = MaterialTheme.colorScheme.surface
+) {
+    CircularProgressIndicator(
+        progress = progress,
+        modifier = modifier.size(ActionProgressIndicatorSize),
+        color = color,
+        strokeWidth = CircularProgressIndicatorStrokeWidth,
+        trackColor = trackColor,
+        strokeCap = DefaultStrokeCap,
+    )
 }
