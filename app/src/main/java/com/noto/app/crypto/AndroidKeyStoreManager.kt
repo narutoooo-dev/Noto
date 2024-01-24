466886bb438626887c33d1bd952fed1a7382d92f
Ali Albaali <ali@albaali.com>
2024-01-24 07:48:57 -0500

    Refactor KeyStoreManager to only handle keys storage

--- app/src/main/java/com/noto/app/crypto/AndroidKeyStoreManager.kt
+++ app/src/main/java/com/noto/app/crypto/AndroidKeyStoreManager.kt
@@ -1,44 +1,31 @@
 package com.noto.app.crypto
 
-import android.os.Build
 import android.security.keystore.KeyProperties
 import android.security.keystore.KeyProtection
-import androidx.annotation.RequiresApi
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 import java.security.KeyStore
 import java.security.KeyStore.ProtectionParameter
 import javax.crypto.SecretKey
 import javax.crypto.spec.SecretKeySpec
 
-class AndroidKeyStoreManager(private val keyStore: KeyStore, private val dataStore: DataStore<Preferences>) : KeyStoreManager {
+class AndroidKeyStoreManager(private val keyStore: KeyStore) : KeyStoreManager {
 
-    override suspend fun storeKek(key: ByteArray, encodedParameters: String) {
+    override suspend fun storeKey(id: String, key: ByteArray) {
         val secretKey = SecretKeySpec(key, Algorithm) as SecretKey
         val entry = KeyStore.SecretKeyEntry(secretKey)
-        keyStore.setEntry(KeyStoreManager.KeyEncryptionKeyId, entry, LoadStoreParameter)
-        dataStore.setEncodedParameters(encodedParameters)
+        keyStore.setEntry(id, entry, LoadStoreParameter)
     }
 
-    override suspend fun getKekEncodedParameters(): String {
-        return dataStore.data
-            .map { it[KekParametersKey].toString() }
-            .first()
+    override suspend fun getKey(id: String): SecretKey? {
+        return keyStore.getKey(id, null) as? SecretKey?
     }
 
-    private suspend fun DataStore<Preferences>.setEncodedParameters(encodedParameters: String) {
-        edit { preferences -> preferences[KekParametersKey] = encodedParameters }
+    override suspend fun deleteKey(id: String) {
+        keyStore.deleteEntry(id)
     }
 
     companion object {
         private const val Algorithm = "AES"
-        private val KekParametersKey = stringPreferencesKey(KeyStoreManager.KeyEncryptionKeyParameters)
 
-        @RequiresApi(Build.VERSION_CODES.M)
         private val LoadStoreParameter: ProtectionParameter = KeyProtection.Builder(KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
             .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
             .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
--- app/src/main/java/com/noto/app/crypto/KeyStoreManager.kt
+++ app/src/main/java/com/noto/app/crypto/KeyStoreManager.kt
@@ -1,14 +1,18 @@
 package com.noto.app.crypto
 
+import javax.crypto.SecretKey
+
 interface KeyStoreManager {
 
-    suspend fun storeKek(key: ByteArray, encodedParameters: String)
+    suspend fun storeKey(id: String, key: ByteArray)
 
-    suspend fun getKekEncodedParameters(): String
+    suspend fun getKey(id: String): SecretKey?
+
+    suspend fun deleteKey(id: String)
 
     companion object {
         const val KeyEncryptionKeyId = "key_encryption_key"
-        const val KeyEncryptionKeyParameters = "key_encryption_key_parameters"
+        const val AutoBackupPasscodeId = "auto_backup_passcode"
     }
 
 }
