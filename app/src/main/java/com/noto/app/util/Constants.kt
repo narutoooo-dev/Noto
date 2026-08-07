38407b43d37d422aef2daebe9c808c9fc75a7039
Narutoooo_Dev <flashstore801@gmail.com>
2026-08-07 05:24:14 +0300

    Update Noto Constants

--- app/src/main/java/com/noto/app/util/Constants.kt
+++ app/src/main/java/com/noto/app/util/Constants.kt
@@ -43,17 +43,17 @@ object Constants {
     }
 
     object Noto {
-        const val Email = "noto@alialbaali.com"
-        const val DeveloperUrl = "https://www.alialbaali.com"
+        const val Email = "flashteam273@gmail.com"
+        const val DeveloperUrl = "https://t.me/d_b_if"
         const val LicenseUrl = "https://www.apache.org/licenses/LICENSE-2.0"
-        const val PlayStoreUrl = "https://play.google.com/store/apps/details?id=com.noto"
-        const val GithubUrl = "https://github.com/alialbaali/Noto"
-        const val TelegramUrl = "https://t.me/notoapp"
+        const val PlayStoreUrl = "https://play.google.com/store/apps/details?id=com.noto.debug"
+        const val GithubUrl = "https://github.com/narutoooo-dev/Noto"
+        const val TelegramUrl = "https://t.me/noto_forum"
         const val PrivacyPolicyUrl = "https://github.com/alialbaali/Noto/blob/master/PrivacyPolicy.md"
         const val GithubIssueUrl = "https://github.com/alialbaali/Noto/issues/new"
         const val ReportIssueEmailSubject = "Issue Regarding Noto"
-        const val GitHubReleasesUrl = "https://github.com/alialbaali/Noto/releases"
-        fun GitHubReleaseUrl(version: String) = "https://github.com/alialbaali/Noto/releases/tag/v$version"
+        const val GitHubReleasesUrl = "https://github.com/narutoooo-dev/Noto/releases"
+        fun GitHubReleaseUrl(version: String) = "https://github.com/narutoooo-dev/Noto/releases/tag/v$version"
 
         fun ReportIssueEmailBody(androidVersion: String, sdkVersion: String, appVersion: String) = """
             Hi there,
